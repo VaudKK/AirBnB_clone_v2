42fa24313164e1abb60bece4ef0fc209403adaed
VaudKK <vaudkeith@gmail.com>
2023-01-06 20:22:05 +0300

    Adding comments to Task 3 sql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,6 @@
+-- creating the hbnb_dv_db database
+-- creating the hbnb_dev user
+-- adding privileges to the user
 CREATE DATABASE IF NOT EXISTS hbnb_dv_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dv_db.* TO 'hbnb_dev'@'localhost';
