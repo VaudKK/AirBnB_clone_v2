7bd9a76b04db5bf7d143ab325c656686507dd0a2
VaudKK <vaudkeith@gmail.com>
2023-01-06 20:24:36 +0300

    Task 3 reordering

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,5 +3,5 @@
 -- adding privileges to the user
 CREATE DATABASE IF NOT EXISTS hbnb_dv_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dv_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dv_db.* TO 'hbnb_dev'@'localhost';
